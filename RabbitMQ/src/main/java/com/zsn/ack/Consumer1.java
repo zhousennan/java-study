1dbfe4ad4e21b5640410e75ff83b5c62cb25e562
zhousennan <zhousennan@ypzdw.com>
2024-05-22 16:26:44 +0800

    add rabbitMQ mode

---
+++ RabbitMQ/src/main/java/com/zsn/ack/Consumer1.java
@@ -0,0 +1,53 @@
+package com.zsn.ack;
+
+import com.rabbitmq.client.CancelCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DeliverCallback;
+import com.zsn.utils.RabbitMqConnection;
+
+import java.io.IOException;
+
+/**
+ * @Author : zhou sen nan
+ * @Date : 2022/2/14  14:35
+ */
+public class Consumer1 {
+    private final  static String QUEUE_NAME = "task_ack";
+
+    public static void main(String[] args) throws IOException {
+        Connection connection = RabbitMqConnection.RabbitMqConnectionUtil();
+        Channel channel = connection.createChannel();
+        //设置不公平分发
+        int prefetchCount= 2;
+        channel.basicQos(prefetchCount);
+        //推送的消息如何进行消费的接口回调
+        DeliverCallback deliverCallback = (consumerTag, delivery) ->
+        {
+            System.out.println("消费者1响应时间较短...");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String message = new String(delivery.getBody());
+            System.out.println("消费者1接收消息："+message);
+            /**
+             * 1.消息标记 tag
+             * 2.是否批量应答未应答消息
+             */
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(),false);
+        };
+        CancelCallback cancelCallback = (consumerTag) -> {
+            System.out.println("消息消费被中断");
+        };
+        System.out.println("1消费者启动等待消费...");
+        /**
+         * 消费者消费消息
+         * 1.消费哪个队列
+         * 2.消费成功之后是否要自动应答 true 代表自动应答 false 手动应答
+         * 3.消费者未成功消费的回调
+         */
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, cancelCallback);
+    }
+}
---
+++ RabbitMQ/src/main/java/com/zsn/exchanges/fanoutExechange/ReceiveLogs02.java
@@ -0,0 +1,39 @@
+package com.zsn.exchanges.fanoutExechange;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DeliverCallback;
+import com.zsn.utils.RabbitMqConnection;
+
+import java.io.IOException;
+
+/**
+ * @Author : zhou sen nan
+ * @Date : 2022/3/8  20:23
+ */
+public class ReceiveLogs02 {
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws IOException {
+        Connection connection = RabbitMqConnection.RabbitMqConnectionUtil();
+        Channel channel = null;
+        channel = connection.createChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        /**
+         * 生成一个临时的队列 队列的名称是随机的
+         * 当消费者断开和该队列的连接时 队列自动删除
+         */
+        String queueName = channel.queueDeclare().getQueue();
+
+//把该临时队列绑定我们的 exchange 其中 routingkey(也称之为 binding key)为空字符串
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+        System.out.println("等待接收消息,把接收到的消息打印在屏幕........... ");
+        DeliverCallback deliverCallback = (consumerTag, delivery) ->
+        {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("ReceiveLogs02控制台打印接收到的消息" + message);
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+    }
+}
